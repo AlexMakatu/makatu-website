80ee6591950e031304981742d6f7731d80386bb0
FTS <devon@fairtradesafaris.com>
2026-04-03 10:44:41 +0200

    Redirect Blogs

--- .history/next.config_20260403104405.ts
+++ .history/next.config_20260403104405.ts
@@ -10,6 +10,18 @@ const nextConfig: NextConfig = {
     ],
   },
 
+  import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+      },
+    ],
+  },
+
   async redirects() {
     return [
       // 🔐 LEGAL PAGES
